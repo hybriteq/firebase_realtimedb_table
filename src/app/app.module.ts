394828c2e6ed2cc3491d5cb35340dd02fdbc6efd
janaka <bjchathuranga@gmail.com>
2018-02-20 17:25:19 +0530

    Init :rocket:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//Import table module
+import { TableModule } from './modules/table/table.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
